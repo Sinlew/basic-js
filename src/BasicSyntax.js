bafaed46ec5b5c44c1dab15df0067cb2e1134244
Alexandr Gavrilenko <acoolfreak@gmail.com>
2022-03-27 17:08:22 +0300

    Задание по JS

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,15 @@
 export function romanToInteger(str) {
-    let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
-    return result;
+    var values = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    var digits = Object.keys(values);
+    var res = 0;
+
+    for (var q = 0; q < str.length; ++q) {
+        if (digits.indexOf(str[q]) < digits.indexOf(str[q + 1])) {
+            res -= values[str[q]];
+        } else {
+            res += values[str[q]];
+        }
+    }
+
+    return res;
 }
